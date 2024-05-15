0cfca4b07c14cd11d90b2eb4e62eac74cf972fe5
Kirk Wang <k3wang@gmail.com>
2024-05-15 10:29:18 -0700

    update engine.rb to specify engine root for migrations

--- lib/hyku_knapsack/engine.rb
+++ lib/hyku_knapsack/engine.rb
@@ -21,7 +21,7 @@ module HykuKnapsack
       # only add the migrations if they are not already copied
       # via the rake task. Allows gem to work both with the install:migrations
       # and without it.
-      if !app.root.to_s.match(root.to_s) &&
+      if !app.root.to_s.match(HykuKnapsack::Engine.root.to_s) &&
          app.root.join('db/migrate').children.none? { |path| path.fnmatch?("*.hyku_knapsack.rb") }
         config.paths["db/migrate"].expanded.each do |expanded_path|
           app.config.paths["db/migrate"] << expanded_path
