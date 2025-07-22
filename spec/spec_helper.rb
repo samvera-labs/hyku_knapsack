b665bb3d763cc47c4e47530e14541d93c7e9d211
Shana Moore <shana.lavina.moore@gmail.com>
2025-07-22 15:21:21 -0700

    remove hyku_specs symlink
    Knapsack should run its specs only, and not be concerned about hyku's specs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,4 @@
 # frozen_string_literal: true
 
-RSpec.configure do |config|
-  # Look for an overriding spec file and skip if it exists
-  config.around do |example|
-    if example.file_path.starts_with?("./spec/hyku_specs") && File.exist?(example.file_path.sub("./spec/hyku_specs", "."))
-      skip "Override exists of this test file in engine."
-    else
-      example.run
-    end
-  end
-end
-
-require File.expand_path("hyku_specs/rails_helper.rb", __dir__)
-require File.expand_path("hyku_specs/spec_helper.rb", __dir__)
+require File.expand_path("../hyrax-webapp/spec/rails_helper.rb", __dir__)
+require File.expand_path("../hyrax-webapp/spec/spec_helper.rb", __dir__)
